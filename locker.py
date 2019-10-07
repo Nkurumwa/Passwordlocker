d26b993b714de85e98e5beaa0abe2ca5fdc6fa13
Nkurumwa <nkurumwa51@gmail.com>
2019-10-07 09:24:51 +0300

    adds credentials class to locker file

--- locker.py
+++ locker.py
@@ -1,1 +1,15 @@
 import pyperclip
+
+class Credentials:
+    '''
+    Class that creates accounts and authenticates the users
+    '''
+    users_list=[]
+
+    def __init__(self, identify, user_name, password):
+        '''
+        Initalizing the variables
+        '''
+        self.identify = identify
+        self.user_name = user_name
+        self.password = password
