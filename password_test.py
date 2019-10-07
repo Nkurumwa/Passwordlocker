e40be01e01d75df54bd8ef0bf88d76ff617a0796
Nkurumwa <nkurumwa51@gmail.com>
2019-10-07 09:45:49 +0300

    tests for classes functionality

--- password_test.py
+++ password_test.py
@@ -1,2 +1,44 @@
 from password import Credentials,UsersData
 import unittest, pyperclip
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines the test cases for creating and authenticating credentials
+    '''
+    def setUp(self):
+        '''
+        Setting up the structure before each test
+        '''
+        self.new_user = Credentials(1,"richie","uiui")
+    
+    def tearDown(self):
+        '''
+        Cleans up after each test has run
+        '''
+        Credentials.users_list = []
+    
+    def test_init(self):
+        '''
+        Test case to test if the case has been initialized properly
+        '''
+        self.assertEqual(self.new_user.identify,1)
+        self.assertEqual(self.new_user.user_name,"mark")
+        self.assertEqual(self.new_user.password,"olol")
+    
+    def test_create(self):
+        '''
+        Testing if the new credential is saved into the list
+        '''
+        self.new_user.create_account()
+        self.assertEqual(len(Credentials.users_list),1)
+    
+    def test_authenticate(self):
+        '''
+        Testing to check if the authenticate function can sign in a user properly
+        '''
+        self.new_user.create_account()
+        test_account = Credentials(1,"Test","Password")
+        test_account.create_account()
+
+        found_user = Credentials.authenticate_account("Test","Password")
+        self.assertEqual(found_user.identify , test_account.identify)
