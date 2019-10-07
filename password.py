1483c06310c7dc533aa98b89c51167809a291bf0
Nkurumwa <nkurumwa51@gmail.com>
2019-10-07 09:38:18 +0300

    creates user class

--- password.py
+++ password.py
@@ -29,3 +29,21 @@ class Credentials:
             if user.user_name == name and user.password == key:
                 return user
         return 0
+
+     class UsersData:
+    '''
+    Class that holds website and password data for the users
+    '''
+    data_list = []
+    def __init__(self,ident,data_id,website,web_key,name):
+        self.ident = ident
+        self.data_id = data_id
+        self.website = website
+        self.web_key = web_key
+        self.name = name
+    
+    def add_password(self):
+        '''
+        creating a method that creates the username and password
+        '''
+        UsersData.data_list.append(self)
